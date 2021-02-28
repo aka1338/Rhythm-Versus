6cfaed642f2e5430cd03aee0d4736a4995b1917f
John Paul Apolinar <john.apolinar@gmail.com>
2021-02-27 19:21:41 -0800

    Reimport of legacy assets
    - Added TextMeshPro
    - Added FMOD

---
+++ Assets/Scripts/BeatSystem.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+class BeatSystem : MonoBehaviour
+{
+    [StructLayout(LayoutKind.Sequential)]
+    class TimelineInfo
+    {
+        public int currentMusicBeat = 0;
+        public FMOD.StringWrapper lastMarker = new FMOD.StringWrapper();
+    }
+
+    TimelineInfo timelineInfo;
+    GCHandle timelineHandle;
+
+    FMOD.Studio.EVENT_CALLBACK beatCallback;
+
+    // beat keeps track of what beat the song is on. 
+    public static int beat;
+
+    // marker is a string that returns most recent marker passed. 
+    public static string marker;
+
+    public void AssignBeatEvent(FMOD.Studio.EventInstance instance)
+    { 
+        timelineInfo = new TimelineInfo();
+        timelineHandle = GCHandle.Alloc(timelineInfo, GCHandleType.Pinned);
+        beatCallback = new FMOD.Studio.EVENT_CALLBACK(BeatEventCallback);
+        instance.setUserData(GCHandle.ToIntPtr(timelineHandle));
+        instance.setCallback(beatCallback, FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_BEAT | FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER);
+    }
+
+    public void StopAndClear(FMOD.Studio.EventInstance instance)
+    {
+        instance.setUserData(IntPtr.Zero);
+        instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        instance.release();
+        timelineHandle.Free();
+    }
+
+    [AOT.MonoPInvokeCallback(typeof(FMOD.Studio.EVENT_CALLBACK))]
+    static FMOD.RESULT BeatEventCallback(FMOD.Studio.EVENT_CALLBACK_TYPE type, IntPtr instancePtr, IntPtr parameterPtr)
+    {
+        FMOD.Studio.EventInstance instance = new FMOD.Studio.EventInstance(instancePtr);
+
+        // Retrieve the user data
+        IntPtr timelineInfoPtr;
+        FMOD.RESULT result = instance.getUserData(out timelineInfoPtr);
+        if (result != FMOD.RESULT.OK)
+        {
+            Debug.LogError("Timeline Callback error: " + result);
+        }
+        else if (timelineInfoPtr != IntPtr.Zero)
+        {
+            // Get the object to store beat and marker details
+            GCHandle timelineHandle = GCHandle.FromIntPtr(timelineInfoPtr);
+            TimelineInfo timelineInfo = (TimelineInfo)timelineHandle.Target;
+
+            switch (type)
+            {
+                case FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_BEAT:
+                    {
+                        var parameter = (FMOD.Studio.TIMELINE_BEAT_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_BEAT_PROPERTIES));
+                        timelineInfo.currentMusicBeat = parameter.beat;
+                        beat = timelineInfo.currentMusicBeat;
+                    }
+                    break;
+                case FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER:
+                    {
+                        var parameter = (FMOD.Studio.TIMELINE_MARKER_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_MARKER_PROPERTIES));
+                        timelineInfo.lastMarker = parameter.name;
+                        marker = timelineInfo.lastMarker;
+                    }
+                    break;
+            }
+        }
+        return FMOD.RESULT.OK;
+    }
+}
