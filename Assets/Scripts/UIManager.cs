fdcfaca5b7a7fcceecdef016829ec8890afef1f9
John Paul Apolinar <john.apolinar@gmail.com>
2021-03-31 12:09:58 -0700

    Refactoring galore
    - Created a base for UIManager
    - Added methods to  GameManager to handle basic gamestate
    - Added methods to Conductor and BeatSystem to interact with the music in the way the Game state requires it to.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public enum Screens 
+    {
+        Pause = 1,
+        Game
+    }
+    // This class should contain enums on the types of screens the player is able to view.
+    // The UI itself should have OnClick() events that point to the GameManager and pass the Minigame Enum that is selected to be played. 
+
+    public static void SwitchScreen(Screens screen)
+    {
+        if (screen == Screens.Pause)
+        {
+            Debug.Log("The game is paused.");
+        }
+        else if (screen == Screens.Game) {
+            Debug.Log("The game is now unpaused"); 
+        }
+    }
+
+}
