4cdddec82064650c3cb3f8e4e459ff3622ae38b7
John Paul Apolinar <john.apolinar@gmail.com>
2021-04-02 12:08:14 -0700

    Update Conductor.cs
    Added some comments to clear up the use of this class.

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Conductor : MonoBehaviour
 {
-    // This class should handle song event instances in order to grab timeline data. There can only be one instance of a song at one time.
-    // We don't need oneshot timeline info data. 
-    // This class dictates whatever should be playing at whatever time, reguardless of it being a oneshot or background music/ambient noise.  
+    // This class should handle song event instances in order to grab timeline data. There can only be one instance of a song tied to the BeatSystem at a time.
+    // TODO: Handling of one shot noises (that don't need to be tied to the BeatSystem)
+    // This class dictates whatever should be playing at whatever time, reguardless of it being a one shot, minigame/menu music, or ambient noise.  
 
     private static FMOD.Studio.EventInstance _instance;
     private static BeatSystem bS;
