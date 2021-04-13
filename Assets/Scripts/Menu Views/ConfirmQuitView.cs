f71565338c59c3658f88b5c8c189bb10cb7c418e
John Paul Apolinar <john.apolinar@gmail.com>
2021-04-13 14:41:08 -0700

    Added some views

---
+++ Assets/Scripts/Menu Views/ConfirmQuitView.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmQuitView : View
+{
+ 
+    public override void Initialize()
+    {
+
+    }
+
+}
--- Assets/Scripts/Menu Views/PauseMenuView.cs
+++ Assets/Scripts/Menu Views/PauseMenuView.cs
@@ -5,10 +5,13 @@ using UnityEngine.UI;
 
 public class PauseMenuView : View
 {
-    [SerializeField] private Button _resumeButton;
+    [SerializeField] private Button _resumeButton, _quitButton, _settingsButton;
     public override void Initialize()
     {
         _resumeButton.onClick.AddListener(() => ViewManager.Show<MinigameView>());
+        _quitButton.onClick.AddListener(() => ViewManager.Show<ConfirmQuitView>());
+        _settingsButton.onClick.AddListener(() => ViewManager.Show<SettingsView>());
+
     }
 
 }
