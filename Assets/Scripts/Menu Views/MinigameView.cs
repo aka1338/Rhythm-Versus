e4685f09098276079b2e14b12278bfb417514643
John Paul Apolinar <john.apolinar@gmail.com>
2021-04-14 21:47:50 -0700

    Revert "Added Minigame View"
    This reverts commit 0b202b485a6cffe16eb36c03841b571a1608f4b6.

--- Assets/Scripts/Menu Views/MinigameView.cs
+++ Assets/Scripts/Menu Views/MinigameView.cs
@@ -1,11 +1,14 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MinigameView : View
 {
+    [SerializeField] private Button _airSlicerButton, _batterUpButton;
     public override void Initialize()
     {
-
+        _airSlicerButton.onClick.AddListener(() => ViewManager.Show<MinigameView>());
+        _batterUpButton.onClick.AddListener(() => Application.Quit());
     }
 }
