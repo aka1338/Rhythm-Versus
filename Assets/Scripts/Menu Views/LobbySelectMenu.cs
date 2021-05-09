aa37cbe7b3781a493ad1fdce37be6e4212e77ac8
Julia Beatriz Ramos <julia.beatriz99@yahoo.com>
2021-05-09 11:48:33 -0700

    implemented chung's network screens

--- Assets/Scripts/Menu Views/LobbySelectMenu.cs
+++ Assets/Scripts/Menu Views/LobbySelectMenu.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 
 public class LobbySelectMenu : View
 {
-    [SerializeField] private Button _createLobbyButton, _joinLobbyButton, _backScreenButton;
+    [SerializeField] private Button _hostLobbyButton, _joinLobbyButton, _backScreenButton;
     public override void Initialize()
     {
         _backScreenButton.onClick.AddListener(() => ViewManager.Show<localVSonlineSelectorView>());
-        _createLobbyButton.onClick.AddListener(() => ViewManager.Show<MinigameSelectView>());
-        _joinLobbyButton.onClick.AddListener(() => ViewManager.Show<MinigameSelectView>());
+        _hostLobbyButton.onClick.AddListener(() => ViewManager.Show<HostLobbyWaitingView>());
+        _joinLobbyButton.onClick.AddListener(() => ViewManager.Show<JoinLobbyWaitingView>());
     }
 }
