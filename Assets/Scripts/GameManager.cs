df6c2aeb6a3d21a6da4e4b959da503cf814ba060
Julia Beatriz Ramos <julia.beatriz99@yahoo.com>
2021-05-09 22:25:46 -0700

    Network fixes with chung's work

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
-{
+{ 
+    public static GameManager instance;
+
+    public static Dictionary<int, PlayerManager> players = new Dictionary<int, PlayerManager>();
+
+    public GameObject localPlayerPrefab;
+    public GameObject playerPrefab;
+
+    public static bool playerControl = false;//enable or disable player control to their character
+
     //TODO: Events that fire off based on how well timed a player's input was checked against the BeatSystem. 
     //The events are: PerfectHit, EarlyHit, LateHit, and Missed.  
 
@@ -30,6 +40,41 @@ public class GameManager : MonoBehaviour
     //public static event NoteTiming LateHit;
 
     // Pausing is disabled in multiplayer 
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void SpawnPlayer(int _id, string _username, Vector3 _position, Quaternion _rotation)
+    {
+        GameObject _player;
+        if (_id == Client.instance.myId)
+        {
+            _player = Instantiate(localPlayerPrefab, _position, _rotation);
+        }
+        else
+        {
+            _player = Instantiate(playerPrefab, _position, _rotation);
+        }
+        _player.GetComponent<PlayerManager>().id = _id;
+        _player.GetComponent<PlayerManager>().username = _username;
+        players.Add(_id, _player.GetComponent<PlayerManager>());
+    }
+
+    public static void EnableControl()
+    {
+        playerControl = true;
+    }
+
     public static bool isPaused = false;
 
     private void Start()
