cf6680617388127a7aeedb16408ee41c21746766
John Paul Apolinar <john.apolinar@gmail.com>
2021-05-13 01:34:31 -0700

    Update UpdateText.cs

--- Assets/Scripts/Debug/UpdateText.cs
+++ Assets/Scripts/Debug/UpdateText.cs
@@ -14,9 +14,9 @@ public class UpdateText : MonoBehaviour
         playerScore.SetText(GameManager.players[1].username + "s score: " + GameManager.players[1].score);
 
         if (GameManager.players.ContainsKey(2)) 
-        player2Score.SetText(GameManager.players[2].username + "s score: " + GameManager.players[1].score);
+        player2Score.SetText(GameManager.players[2].username + "s score: " + GameManager.players[2].score);
 
         if (GameManager.players.ContainsKey(3))
-        player3Score.SetText(GameManager.players[3].username + "s score: " + GameManager.players[1].score);
+        player3Score.SetText(GameManager.players[3].username + "s score: " + GameManager.players[3].score);
     }
 }
