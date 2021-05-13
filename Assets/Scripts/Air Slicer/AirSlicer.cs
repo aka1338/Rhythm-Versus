56b9a86537d36909f6e438b7f38c0c99a0e2cadb
John Paul Apolinar <john.apolinar@gmail.com>
2021-05-12 23:32:52 -0700

    crying, really hard right now

--- Assets/Scripts/Air Slicer/AirSlicer.cs
+++ Assets/Scripts/Air Slicer/AirSlicer.cs
@@ -116,7 +116,8 @@ public class AirSlicer : MonoBehaviour
             
             else 
             {
-                Instantiate(prefab[5], new Vector3(referencePoint.transform.position.x, referencePoint.transform.position.y+2, 0f), Quaternion.identity, referencePoint);
+                Destroy(Instantiate(prefab[5], new Vector3(currentVeggieNote.transform.position.x, currentVeggieNote.transform.position.y, 0f), Quaternion.identity, referencePoint), 1f);
+                //Instantiate(prefab[5], new Vector3(referencePoint.transform.position.x, referencePoint.transform.position.y+2, 0f), Quaternion.identity, referencePoint);
             }
 
         }
