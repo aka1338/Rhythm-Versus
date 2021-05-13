c89f64214a3ee223f9eeeb8a682f17e89dc94ba7
John Paul Apolinar <john.apolinar@gmail.com>
2021-05-12 23:02:29 -0700

    does this solve the issue?
    if not, I'm gonna shoot myself

--- Assets/Scripts/Air Slicer/AirSlicer.cs
+++ Assets/Scripts/Air Slicer/AirSlicer.cs
@@ -105,7 +105,6 @@ public class AirSlicer : MonoBehaviour
                 Destroy(Instantiate(prefab[5], new Vector3(currentVeggieNote.transform.position.x, currentVeggieNote.transform.position.y, 0f), Quaternion.identity, referencePoint), 1f);
             }
 
-            Debug.Log("This should work!");
             Debug.Log(GameManager.isLocal); 
 
             if (!GameManager.isLocal) 
